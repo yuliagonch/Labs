a2b7a199984c2eb4d9f56dbf14b33cb3096ac982
yuliagonch <115131713+yuliagonch@users.noreply.github.com>
2022-10-19 22:03:49 -0700

    Lab3

---
+++ EventLab/Starter/Exercise 1/GradesPrototype/Views/LogonPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for LogonPage.xaml
+    /// </summary>
+    public partial class LogonPage : UserControl
+    {
+        public LogonPage()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Members
+        // TODO: Exercise 1: Task 2a: Define the LogonSuccess event handler
+        public event EventHandler LogonSuccess;
+        #endregion
+
+        #region Logon Validation
+
+        // TODO: Exercise 1: Task 2b: Implement the Logon_Click event handler for the Logon button
+        // Simulate logging on (no validation or authentication performed yet)
+        private void Logon_Click(object sender, RoutedEventArgs e)
+        {
+            // Save the username and role (type of user) specified on the form in the   global context
+            SessionContext.UserName = username.Text;
+            SessionContext.UserRole = (bool)userrole.IsChecked ? Role.Teacher : Role.Student;
+
+            // If the role is Student, set the CurrentStudent property in the global    context to a dummy student; Eric Gruber
+            if (SessionContext.UserRole == Role.Student)
+            {
+                SessionContext.CurrentStudent = "Eric Gruber";
+            }
+
+            // Raise the LogonSuccess event
+            if (LogonSuccess != null)
+            {
+                LogonSuccess(this, null);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ EventLab/Starter/Exercise 2/GradesPrototype/Data/Grade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesPrototype.Data
+{
+    // Types of user
+    public enum Role { Teacher, Student };
+
+    // TODO: Exercise 2: Task 1a: Create the Grade struct
+    public struct Grade
+    {
+        public int StudentID { get; set; }
+        public string AssessmentDate { get; set; }
+        public string SubjectName { get; set; }
+        public string Assessment { get; set; }
+        public string Comments { get; set; }
+    }
+
+    // TODO: Exercise 2: Task 1b: Create the Student struct
+    public struct Student
+    {
+        public int StudentID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int TeacherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    // TODO: Exercise 2: Task 1c: Create the Teacher struct
+    public struct Teacher
+    {
+        public int TeacherID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Class { get; set; }
+    }
+}
